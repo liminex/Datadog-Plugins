626afe7fceaa5bd67725917471a021372cdd12d0
Advait Shinde <advait.shinde@gmail.com>
2014-12-22 13:21:34 -0800

    Use yaml config for various fpm settings.

--- php-fpm/php-fpm.py
+++ php-fpm/php-fpm.py
@@ -17,14 +17,18 @@ class details(AgentCheck):
     'total processes': 'php-fpm.total processes',
     'max active processes': 'php-fpm.max active processes',
     'max children reached': 'php-fpm.max children reached',
-    }
+  }
 
- def check(self,instance):
-    default_timeout = self.init_config.get('default_timeout', 5)
-    os.environ["SCRIPT_NAME"] = "/status"
-    os.environ["SCRIPT_FILENAME"] = "/status"
-    os.environ["REQUEST_METHOD"] = "GET"
-    req = subprocess.Popen(["cgi-fcgi", "-bind", "-connect", "127.0.0.1:9000"], stdout=subprocess.PIPE).communicate()[0]
+  def check(self, instance):
+    default_timeout = instance.get('default_timeout', 5)
+    fpm_status_uri = instance.get('fpm_status_uri', '/status')
+    fpm_request_method = instance.get('fpm_request_method', 'GET')
+    fpm_hostname = instance.get('fpm_hostname', '127.0.0.1')
+    fpm_port = instance.get('fpm_port', 9000)
+    os.environ["SCRIPT_NAME"] = os.environ["SCRIPT_FILENAME"] = fpm_status_uri
+    os.environ["REQUEST_METHOD"] = fpm_request_method
+    host_and_port = "%s:%d" % (fpm_hostname, fpm_port)
+    req = subprocess.Popen(["cgi-fcgi", "-bind", "-connect", host_and_port], stdout=subprocess.PIPE).communicate()[0]
     metric_count = 0
     line = req
     for queue in line.split('\n'):
